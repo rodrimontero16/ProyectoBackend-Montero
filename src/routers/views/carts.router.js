0af7fae1735cf35c78f84d4da057f636f3195f06
rodrimontero16 <rodrimontero16@gmail.com>
2023-11-16 16:44:11 -0300

    v1.6.4

--- src/routers/views/carts.router.js
+++ src/routers/views/carts.router.js
@@ -4,6 +4,10 @@ import CartManager from '../../dao/CartManager.js';
 
 const router = Router();
 
+function calcularTotal(products) {
+    return products.reduce((total, product) => total + (product.totalPrice || 0), 0);
+}
+
 router.get('/carts/:cid', async (req, res) => {
     try {
         const { cid } = req.params; 
@@ -12,7 +16,8 @@ router.get('/carts/:cid', async (req, res) => {
         const products = cart.products.map(e => {
             return {...e.product._doc, quantity: e.quantity, cartId, totalPrice:e.quantity*e.product.price}
         })
-        res.render('carts', {products, titlePage: 'Carrito', style: 'carts.css'})
+        const totalCompra = calcularTotal(products);
+        res.render('carts', {products, totalCompra, titlePage: 'Carrito', style: 'carts.css'})
     } catch (error) {
         res.status(error.statusCode || 500).json({ message: error.message });
     }
