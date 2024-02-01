0f0f049b980c353a64ad807d867c8866546e80c7
rodrimontero16 <rodrimontero16@gmail.com>
2024-02-01 17:54:04 -0300

    v3.2.0
    Documentando

--- src/app.js
+++ src/app.js
@@ -9,7 +9,9 @@ import cookieParser from 'cookie-parser';
 import config from './config/config.js';
 import cors from 'cors';
 import ErrorHandler from './middlewares/ErrorHandler.js';
-import { addLogger } from './config/logger.js'
+import { addLogger } from './config/logger.js';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi  from 'swagger-ui-express';
 
 //Routes
 import productsApiRouter from './routers/api/products.router.js';
@@ -28,6 +30,22 @@ app.use(cookieParser(COOKIE_SECRET));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../../public')));
+
+//swagger
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.1',
+        info: {
+            title: 'Proyecto Backend',
+            description: 'Esta es la documentación del proyecto final del curso de Backend.',
+        },
+        },
+        apis: [path.join(__dirname, '..', 'docs', '**', '*.yaml')],
+    };
+
+const specs = swaggerJsDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 
 //cors
 const corsOptions = {
