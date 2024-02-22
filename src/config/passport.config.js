af5c82f02708a49240ec6428db9bbe3fa235ed82
rodrimontero16 <rodrimontero16@gmail.com>
2024-02-22 17:33:43 -0300

    Update passport.config.js

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -9,7 +9,7 @@ import config from './config.js';
 const githubOptions = {
     clientID: config.github.clientID, 
     clientSecret: config.github.clientSecret, 
-    callbackURL: "http://localhost:8080/api/auth/github/callback", 
+    callbackURL: process.env.RAILWAY_ENV === 'production' ? "/api/auth/github/callback" : "http://localhost:8080/api/auth/github/callback", 
 }
 
 function cookieExtractor(req) {
